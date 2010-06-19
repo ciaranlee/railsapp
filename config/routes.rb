fd9a1182470b807c13e09d22c136acf9be3c4c9f
Ciaran Lee <ciaran.lee@gmail.com>
2010-06-19 01:20:09 +0100

    added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Surf::Application.routes.draw do |map|
+  get "home/index"
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Surf::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
